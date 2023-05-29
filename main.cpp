d42c2deaaaf027f880416ed5288dbbe55c4b63f9
Daniel Aguilera <jdas_9920@hotmail.com>
2023-05-29 15:22:13 -0700

    refresco de pantalla_
    cambie las funciones para poder manipular todo independientemente

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
+#include <locale.h>
 #include <time.h>
 
 using namespace std;
 
+int matriz[5][4] = {0};
+int numero = 0;
+int pares = 0;
 
-void crearMatriz(){
+void crearMatriz(int userx, int usery){
 
-    const int filas = 4;
-    const int columnas = 5;
-    const int totalPares = 10;
-    int matriz[filas][columnas] = {0};
-    int numero = 0;
-    int pares = 0;
 
     //llenar arreglo
     for(int i = 0; i < 5; i++){
@@ -25,13 +24,12 @@ void crearMatriz(){
             }
         }
     }
+}
 
-    int casilla = 1;
-    //imprimir arreglo
+void imprimirMatriz(){
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 4; j++){
             cout <<matriz[i][j]<<" ";
-            casilla++;
         }
         cout << endl;
     }
@@ -40,13 +38,20 @@ void crearMatriz(){
 int main()
 {
     int userx = 0, usery = 0;
-    int casillaUser;
-    
-    crearMatriz();
-    cout << "Ingrese la fila: ";
-    cin >> userx;
-    cout << "Ingrese la columna: ";
-    cin >> usery;
+    bool userState = true;
+
+    crearMatriz(userx, usery);
+
+    while(userState){
+        imprimirMatriz();
+        cout << "Ingrese la fila: ";
+        cin >> userx;
+        cout << "Ingrese la columna: ";
+        cin >> usery;
+
+        system("cls");
+    }
+
 
     return 0;
 }
