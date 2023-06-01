b4a8cbd00ea2ad3bb19d40fbccb7c0b11296c95c
Gamma <114522363+Gamma-code@users.noreply.github.com>
2023-06-01 16:48:45 -0700

    Actualizado con menu por tilin.cpp

--- Actualizado con menu.cpp
+++ Actualizado con menu.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
-#include <locale.h>
-#include <time.h>
 
 using namespace std;
 
-int matriz[5][4] = {0};
-int numero = 0;
-int pares = 0;
-int parfound = 0;
-int numIgual = 0;
-int vueltax, vueltay;
-int puntaje = 0;
+const int FILAS = 5;
+const int COLUMNAS = 4;
 
-void crearMatriz(int userx, int usery){
+int matriz[FILAS][COLUMNAS] = {0};
+bool revelado[FILAS][COLUMNAS] = {false};
+int paresEncontrados = 0;
+int numAnteriorFila = -1;
+int numAnteriorColumna = -1;
 
-    // llenar arreglo
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 4; j++){
+void crearMatriz() {
+    int numero = 0;
+    int pares = 0;
+
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
             matriz[i][j] = numero;
             pares++;
-            if(pares == 2){
+            if (pares == 2) {
                 numero++;
                 pares = 0;
             }
@@ -30,30 +30,33 @@ void crearMatriz(int userx, int usery){
 
     // mezclar matriz
     srand(time(0));
-    int totalElementos = 5 * 4;
-    for(int i = totalElementos - 1; i > 0; i--){
+    int totalElementos = FILAS * COLUMNAS;
+    for (int i = totalElementos - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-        int temp = matriz[i / 4][i % 4];
-        matriz[i / 4][i % 4] = matriz[j / 4][j % 4];
-        matriz[j / 4][j % 4] = temp;
+        int temp = matriz[i / COLUMNAS][i % COLUMNAS];
+        matriz[i / COLUMNAS][i % COLUMNAS] = matriz[j / COLUMNAS][j % COLUMNAS];
+        matriz[j / COLUMNAS][j % COLUMNAS] = temp;
     }
 }
 
-void imprimirMatriz(int userx, int usery, int vueltax, int vueltay, int numIgual, int parfound){
-    //Hacer que se impriman los asteriscos y se actualice el mapa 
-    //cuando se encuentre el par, que se muestren los dos numeros encontrados
-    //También debemos hacer que cuando seleccionemos un numero, se muestre
-       for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 4; j++){
-            if(puntaje == 0){
-               cout <<" * ";
-            }else if(numIgual == matriz[userx][usery] && parfound == 2 && (vueltax != userx && vueltay != usery)){
-               cout <<matriz[i][j] << " ";
+void imprimirMatriz() {
+    cout << "    ";
+    for (int j = 0; j < COLUMNAS; j++) {
+        cout << j << "   ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < FILAS; i++) {
+        cout << i << " | ";
+        for (int j = 0; j < COLUMNAS; j++) {
+            if (revelado[i][j]) {
+                cout << matriz[i][j] << " ";
+            } else {
+                cout << "* ";
             }
         }
         cout << endl;
     }
-
 }
 
 void mostrarPortada() {
@@ -79,56 +82,61 @@ void mostrarInstrucciones() {
     system("cls");
 }
 
-void juegoMatriz(int userx, int usery){
-
-
-    if(userx < 5 && usery < 4){
-        cout<<"Usted a seleccionado el numero: "<<matriz[userx][usery]<<endl;
-        parfound++;
-
-    if(parfound == 1){
-        cout<<"Ingrese otras coordenadas para encontrar el par!"<<endl<<endl;
-        numIgual = matriz[userx][usery];
-        vueltax = userx;
-        vueltay = usery;
-
-    }else if(numIgual == matriz[userx][usery] && parfound == 2 && (vueltax != userx && vueltay != usery)){
-        cout << "USTED HA ENCONTRADO UN PAR!"<<endl;
-        numIgual = 10;
-        parfound = 0;
-    }else if(matriz[userx][usery] != numIgual && parfound == 2){
-        cout <<"AQUI NO ESTA EL PAR!"<<endl;
-        parfound = 0;
+void juegoMatriz(int fila, int columna) {
+    if (fila >= 0 && fila < FILAS && columna >= 0 && columna < COLUMNAS) {
+        if (revelado[fila][columna]) {
+            cout << "Este número ya está revelado. Por favor, selecciona otro." << endl;
+        } else {
+            revelado[fila][columna] = true;
+            imprimirMatriz();
+            if (numAnteriorFila == -1 && numAnteriorColumna == -1) {
+                numAnteriorFila = fila;
+                numAnteriorColumna = columna;
+            } else {
+                if (matriz[fila][columna] == matriz[numAnteriorFila][numAnteriorColumna]) {
+                    cout << "¡Has encontrado un par! Los números son iguales." << endl;
+                    paresEncontrados++;
+                } else {
+                    revelado[fila][columna] = false;
+                    revelado[numAnteriorFila][numAnteriorColumna] = false;
+                    cout << "Los números no son iguales. Inténtalo de nuevo." << endl;
+                }
+                numAnteriorFila = -1;
+                numAnteriorColumna = -1;
+            }
+        }
+    } else {
+        cout << "Selecciona una posición válida." << endl;
     }
-
-    }else if(userx > 5 && usery > 4){
-        cout <<"Ingrese coordenadas del 0 al 3 para la fila y del 0 al 4 para la columna"<<endl;
-    }
-
 }
 
-int main()
-{
-    int userx = 0, usery = 0;
-    bool userState = true;
+int main() {
+    int fila = 0, columna = 0;
+    bool juegoTerminado = false;
 
     mostrarPortada();
     mostrarInstrucciones();
 
-    crearMatriz(userx, usery);
+    crearMatriz();
 
-    while(userState){
-        imprimirMatriz(userx, usery, vueltax, vueltay, numIgual, parfound);
-        cout << "Ingrese la columna: ";
-        cin >> userx;
+    while (!juegoTerminado) {
+        imprimirMatriz();
         cout << "Ingrese la fila: ";
-        cin >> usery;
+        cin >> fila;
+        cout << "Ingrese la columna: ";
+        cin >> columna;
 
-        juegoMatriz(userx, usery);
+        juegoMatriz(fila, columna);
 
         system("pause");
         system("cls");
+
+        if (paresEncontrados == (FILAS * COLUMNAS) / 2) {
+            cout << "¡Felicidades! ¡Has encontrado todos los pares!" << endl;
+            juegoTerminado = true;
+        }
     }
 
     return 0;
 }
+
