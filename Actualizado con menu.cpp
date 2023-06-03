3a8e5e1287791cdbf00fb0dec7ccb4f9042d31d7
Gamma <114522363+Gamma-code@users.noreply.github.com>
2023-06-03 11:08:15 -0700

    Actualizado con menu colores.cpp

--- Actualizado con menu.cpp
+++ Actualizado con menu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <Windows.h>
 
 using namespace std;
 
@@ -13,15 +14,19 @@ int paresEncontrados = 0;
 int numAnteriorFila = -1;
 int numAnteriorColumna = -1;
 
-void crearMatriz() {
+void crearMatriz()
+{
     int numero = 0;
     int pares = 0;
 
-    for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
+    for (int i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLUMNAS; j++)
+        {
             matriz[i][j] = numero;
             pares++;
-            if (pares == 2) {
+            if (pares == 2)
+            {
                 numero++;
                 pares = 0;
             }
@@ -31,7 +36,8 @@ void crearMatriz() {
     // mezclar matriz
     srand(time(0));
     int totalElementos = FILAS * COLUMNAS;
-    for (int i = totalElementos - 1; i > 0; i--) {
+    for (int i = totalElementos - 1; i > 0; i--)
+    {
         int j = rand() % (i + 1);
         int temp = matriz[i / COLUMNAS][i % COLUMNAS];
         matriz[i / COLUMNAS][i % COLUMNAS] = matriz[j / COLUMNAS][j % COLUMNAS];
@@ -39,19 +45,30 @@ void crearMatriz() {
     }
 }
 
-void imprimirMatriz() {
+void imprimirMatriz()
+{
     cout << "    ";
-    for (int j = 0; j < COLUMNAS; j++) {
-        cout << j << "   ";
+    for (int j = 0; j < COLUMNAS; j++)
+    {
+        cout << j << " ";
     }
     cout << endl;
 
-    for (int i = 0; i < FILAS; i++) {
+    for (int i = 0; i < FILAS; i++)
+    {
         cout << i << " | ";
-        for (int j = 0; j < COLUMNAS; j++) {
-            if (revelado[i][j]) {
+        for (int j = 0; j < COLUMNAS; j++)
+        {
+            if (revelado[i][j])
+            {
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                int color = matriz[i][j] % 15 + 1;
+                SetConsoleTextAttribute(hConsole, color);
                 cout << matriz[i][j] << " ";
-            } else {
+                SetConsoleTextAttribute(hConsole, 15);
+            }
+            else
+            {
                 cout << "* ";
             }
         }
@@ -59,44 +76,89 @@ void imprimirMatriz() {
     }
 }
 
-void mostrarPortada() {
-    cout << "==================================" << endl;
-    cout << "             MEMORAMA             " << endl;
-    cout << "==================================" << endl;
-    cout << "    Realizado por: Dani y Gama    " << endl;
-    cout << "==================================" << endl;
+void mostrarPortada()
+{
+    srand(time(0));
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    int color1 = rand() % 15 + 1;
+    int color2 = rand() % 15 + 1;
+
+    SetConsoleTextAttribute(hConsole, color1);
+    cout << "=========================================================" << endl;
+    cout << "                          MEMORAMA                       " << endl;
+    cout << "=========================================================" << endl;
+    SetConsoleTextAttribute(hConsole, color2);
+    cout << " Realizado por: Daniel Aguilera & \n                                  Brayan Torres" << endl;
+    SetConsoleTextAttribute(hConsole, 15);
+    cout << "=========================================================" << endl;
     cout << endl;
     cout << "Presiona ENTER para comenzar el juego..." << endl;
     cin.ignore();
 }
 
-void mostrarInstrucciones() {
+
+void mostrarInstrucciones()
+{
+    srand(time(0));
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    int color1 = rand() % 15 + 1;
+    int color2 = rand() % 15 + 1;
+
+    SetConsoleTextAttribute(hConsole, color1);
+
     system("cls");
-    cout << "==================================" << endl;
-    cout << "          Instrucciones           " << endl;
-    cout << "==================================" << endl;
-    cout << "Instrucciones del juego..." << endl;
+
+
+
+    cout << "=================================================================================" << endl;
+    cout << "                                   Instrucciones                                 " << endl;
+    cout << "=================================================================================" << endl;
+
+    SetConsoleTextAttribute(hConsole, 15);
+    cout << "El objetivo del juego es encontrar todos los pares de números.                   " << endl;
+    cout << "En cada turno, selecciona una posición en la matriz ingresando su fila y columna." << endl;
+    cout << "Se revelará el número en esa posición.                                           " << endl;
+    cout << "Luego, selecciona otra posición para intentar encontrar el par del número anterior," << endl;
+    cout << "Si los números son iguales, has encontrado un par y se mantendrán revelados." << endl;
+    cout << "Si los números son diferentes, se volverán a ocultar," << endl;
+    cout << "Continúa hasta encontrar todos los pares." << endl;
     cout << endl;
     cout << "Presiona ENTER para volver al juego..." << endl;
+
     cin.ignore();
     system("cls");
 }
 
-void juegoMatriz(int fila, int columna) {
-    if (fila >= 0 && fila < FILAS && columna >= 0 && columna < COLUMNAS) {
-        if (revelado[fila][columna]) {
+void juegoMatriz(int fila, int columna)
+{
+    if (fila >= 0 && fila < FILAS && columna >= 0 && columna < COLUMNAS)
+    {
+        if (revelado[fila][columna])
+        {
             cout << "Este número ya está revelado. Por favor, selecciona otro." << endl;
-        } else {
+        }
+        else
+        {
             revelado[fila][columna] = true;
             imprimirMatriz();
-            if (numAnteriorFila == -1 && numAnteriorColumna == -1) {
+            if (numAnteriorFila == -1 && numAnteriorColumna == -1)
+            {
                 numAnteriorFila = fila;
                 numAnteriorColumna = columna;
-            } else {
-                if (matriz[fila][columna] == matriz[numAnteriorFila][numAnteriorColumna]) {
+            }
+            else
+            {
+                if (matriz[fila][columna] == matriz[numAnteriorFila][numAnteriorColumna])
+                {
+                    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                    int color = matriz[fila][columna] % 15 + 1;
+                    SetConsoleTextAttribute(hConsole, color);
                     cout << "¡Has encontrado un par! Los números son iguales." << endl;
+                    SetConsoleTextAttribute(hConsole, 15);
                     paresEncontrados++;
-                } else {
+                }
+                else
+                {
                     revelado[fila][columna] = false;
                     revelado[numAnteriorFila][numAnteriorColumna] = false;
                     cout << "Los números no son iguales. Inténtalo de nuevo." << endl;
@@ -105,12 +167,16 @@ void juegoMatriz(int fila, int columna) {
                 numAnteriorColumna = -1;
             }
         }
-    } else {
+    }
+    else
+    {
         cout << "Selecciona una posición válida." << endl;
     }
 }
 
-int main() {
+int main()
+{
+    setlocale(LC_ALL, "");
     int fila = 0, columna = 0;
     bool juegoTerminado = false;
 
@@ -119,7 +185,8 @@ int main() {
 
     crearMatriz();
 
-    while (!juegoTerminado) {
+    while (!juegoTerminado)
+    {
         imprimirMatriz();
         cout << "Ingrese la fila: ";
         cin >> fila;
@@ -131,16 +198,28 @@ int main() {
         system("pause");
         system("cls");
 
-        if (paresEncontrados == (FILAS * COLUMNAS) / 2) {
-            cout << "¡Felicidades! ¡Has encontrado todos los pares!" << endl;
+        if (paresEncontrados == (FILAS * COLUMNAS) / 2)
+        {
+            cout << "¡Felicidades! ¡Has encontrado todos los pares! " << endl;
+                string indent = "                                        ";
+                cout << indent << "       \\`\"-. " << endl;
+                cout << indent << "        )  _`-. " << endl;
+                cout << indent << "       ,  : `. \\ " << endl;
+                cout << indent << "       : _   '  \\ " << endl;
+                cout << indent << "       ; *` _.   `--._ " << endl;
+                cout << indent << "       `-.-'          `-. " << endl;
+                cout << indent << "         |       `       `. " << endl;
+                cout << indent << "         :.       .        \\ " << endl;
+                cout << indent << "         | \\  .   :   .-'   . " << endl;
+                cout << indent << "         :  )-.;  ;  /      : " << endl;
+                cout << indent << "         :  ;  | :  :       ;-. " << endl;
+                cout << indent << "         ; /   : |`-:     _ `- ) " << endl;
+                cout << indent << "      ,-' /  ,-' ; .-`- .' `--' " << endl;
+                cout << indent << "      `--'   `---' `---' " << endl;
             juegoTerminado = true;
         }
     }
 
     return 0;
 }
-
-
-
-
 
